eb5934dbc5fc22189a32179e310f2c33d5d08b76
PJAlley <timothy.willy@gmail.com>
2021-12-29 20:08:28 -0500

    Addded user page.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -21,6 +21,7 @@ def index():
     ]
     return render_template('index.html', title='Home Page', posts = posts)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -39,10 +40,12 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/logout')
+
+@app.get('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -57,3 +60,14 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)

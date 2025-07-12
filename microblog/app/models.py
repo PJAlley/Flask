b896766dec8f929286632b3fcc94dfe6b6f640fa
PJAlley <timothy.willy@gmail.com>
2025-07-12 17:28:19 -0400

    Added avatar.

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -2,6 +2,7 @@ from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +16,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size=80):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        avatar_url = f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
+        return avatar_url
 
     def __repr__(self):
         return '<User {}: {}>'.format(self.username, self.email)
